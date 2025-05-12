2dfac8d3701a318f5c94edf1d06c2ef88720e24f
nasghoi <nasrullahjamsarim@gmail.com>
2025-05-12 09:28:39 +0800

    update: variables, comments, assign by ref, data types

--- curriculum.js
+++ curriculum.js
@@ -12,17 +12,67 @@
 // -- Console.log()
 console.log("Hello World");
 // -- Variables
+const mySentence = "hello world";
+
+let numberOfEggs = 4;
+numberOfEggs = 4;
+
+console.log(numberOfEggs + 6, mySentence);
 // -- Code comments
+// this is a comment
+/* this is a comment */
+
 // -- Assignment by reference
+let newNumberOfEggs = numberOfEggs;
 
 // 4.0 Data Types (init / read / write)
 // -- Strings
+
+let myString = "hello my name is nas";
+let extendedString = myString + " and I am a software engineer";
+console.log(extendedString);
+
+console.log(myString[4]);
+
 // -- Numbers
+
+const favNumber = 10;
 // -- Arrays
+
+const groceryList = ["eggs", "milk", "bread"];
+groceryList[0] = "cheese"; // write
+console.log(groceryList);
+
 // -- Objects
+const dictionary = {
+    ocean: 'A body of water between countries',
+    sea: 'A different body of water',
+    myFavNumber: 10,
+}
+
+const user = {
+    name: 'Nas',
+    password: '******',
+}
+
+console.log(user.name); //access using dot notation
+console.log(dictionary['ocean']); //access using bracket notation
+
+user.name = 'Nasir'; // write new value
+console.log(user.name); //access using dot notation
+
+dictionary['ocean'] = 'A large body of salt water'; // write new value
+console.log(dictionary['ocean']); //access using bracket notation
+
 // -- Null
+const unknown = null; // null is an object
 // -- Undefined
+const undefinedValue = undefined; // undefined is a type
+console.log(unknown, undefinedValue);
+
 // -- Booleans (true/false)
+const isNas = true;
+let isAlive = false;
 
 // 5.0 Recap
 
